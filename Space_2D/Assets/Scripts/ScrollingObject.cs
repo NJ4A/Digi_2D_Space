46cacb7b3edb61b2a067ce3c73f5bcbc29179c8c
NJ4A <66083970+NJ4A@users.noreply.github.com>
2021-09-11 18:55:55 -0400

    Merge in Space BG
    Didn't take for some reason.

--- Space_2D/Assets/Scripts/ScrollingObject.cs
+++ Space_2D/Assets/Scripts/ScrollingObject.cs
@@ -4,28 +4,6 @@ using UnityEngine;
 
 public class ScrollingObject : MonoBehaviour
 {
-    /*private Rigidbody2D rigidBody;
-    private float m_speed = -.5f;
-    [SerializeField]  private bool m_stopScrolling;
-
-    private float camLocX, camLocY, camSpdX, camSpdY;
-
-    void Start()
-    {
-        rigidBody = GetComponent<Rigidbody2D>();
-        rigidBody.velocity = new Vector2(m_speed, m_speed);
-    }
-
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        if (m_stopScrolling)
-            rigidBody.velocity = Vector2.zero;
-        else
-            rigidBody.velocity = new Vector2(m_speed, m_speed);
-    }
-    */
-
     public float speed = 1;
     private RectTransform rt;
     private void Start()
