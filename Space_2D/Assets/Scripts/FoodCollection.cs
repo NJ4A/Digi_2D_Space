8b7676f1c61a4c335ce386ea8f532beae1980f59
Shawn <shawnallday130@gmail.com>
2021-09-11 16:49:18 -0400

    Basic level scene.

---
+++ Space_2D/Assets/Scripts/FoodCollection.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodCollection : MonoBehaviour
+{
+    public GameObject food;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        food = food.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
