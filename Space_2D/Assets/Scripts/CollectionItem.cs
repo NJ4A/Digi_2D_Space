5e31baf7f09b26061ec3e9d8f66580681fddbb76
michaelbianchini <mikehotbottoms@gmail.com>
2021-09-14 21:20:27 -0400

    Added crunch!

--- Space_2D/Assets/Scripts/CollectionItem.cs
+++ Space_2D/Assets/Scripts/CollectionItem.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class CollectionItem : MonoBehaviour
 {
+    private GameObject player;
+    private AudioSource[] sounds;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             ManagerClass.updateSpeed = true;
             ManagerClass.itemsCollected++;
+            player = GameObject.FindWithTag("Player");
+            sounds = player.GetComponents<AudioSource>();
+            sounds[1].Play();
             Destroy(gameObject);
         }
     }
